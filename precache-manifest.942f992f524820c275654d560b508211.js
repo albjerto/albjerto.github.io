e7c311c3196df24f0d22a7d239f2de7a078edde4
Alberto Jesu <alberto.jesu@studio.unibo.it>
2020-04-02 21:17:06 +0200

    Updates

---
+++ precache-manifest.942f992f524820c275654d560b508211.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f48616ee19e100b94bdcec053c34ca",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "0a32a33624eb5668c34b",
+    "url": "/personal-website/static/css/main.4e9eb2c7.chunk.css"
+  },
+  {
+    "revision": "7c57426473973c8ecd7e",
+    "url": "/personal-website/static/js/2.77a3851e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.77a3851e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a32a33624eb5668c34b",
+    "url": "/personal-website/static/js/main.1620790a.chunk.js"
+  },
+  {
+    "revision": "8c4e698560fc63c2bbff",
+    "url": "/personal-website/static/js/runtime-main.104b7082.js"
+  },
+  {
+    "revision": "882e172a7debd9947571a3e81ed78300",
+    "url": "/personal-website/static/media/box_1.882e172a.jpg"
+  },
+  {
+    "revision": "8dcad8391c7fc061be2ac06c4e9a4a6a",
+    "url": "/personal-website/static/media/download.8dcad839.png"
+  },
+  {
+    "revision": "9b747ed9a609e9f3dbb572dd3d126315",
+    "url": "/personal-website/static/media/edil_1.9b747ed9.JPG"
+  },
+  {
+    "revision": "2bce1f707e0a62427aebbe90c58cffd3",
+    "url": "/personal-website/static/media/edil_2.2bce1f70.JPG"
+  },
+  {
+    "revision": "4cfb3a7b244c1f63e05fc57bb5893ff9",
+    "url": "/personal-website/static/media/edil_3.4cfb3a7b.JPG"
+  },
+  {
+    "revision": "9c9d986155779bf73970d170fca487de",
+    "url": "/personal-website/static/media/fabio_1.9c9d9861.jpg"
+  },
+  {
+    "revision": "d1e4cc3d2054cd43105b2e753d48e30e",
+    "url": "/personal-website/static/media/koine_1.d1e4cc3d.JPG"
+  },
+  {
+    "revision": "7369de125d7b0020b44c78ad85dab179",
+    "url": "/personal-website/static/media/koine_2.7369de12.JPG"
+  },
+  {
+    "revision": "2845525ac348f4d76b85866a97aa0be8",
+    "url": "/personal-website/static/media/koine_3.2845525a.JPG"
+  },
+  {
+    "revision": "a24fd0128c031d5cb6fa9bdee5c9e465",
+    "url": "/personal-website/static/media/mitm_1.a24fd012.JPG"
+  }
+]);
